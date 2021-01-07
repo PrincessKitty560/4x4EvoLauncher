e30c45202892f433385034bd577274238126a794
FuzzyFish <ionesqueish@gmail.com>
2021-01-06 23:28:10 -0500

    UI: Started work on game settings form

---
+++ 4x4EvoLauncher/4x4Evo_Launcher/GameSettings.Designer.cs
@@ -0,0 +1,414 @@
+﻿
+namespace _4x4Evo_Launcher
+{
+    partial class GameSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameSettings_TabCtrl = new System.Windows.Forms.TabControl();
+            this.GameSettings_Tab_Graphics = new System.Windows.Forms.TabPage();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.GameSettings_Tab_Sound = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.GameSettings_TabCtrl.SuspendLayout();
+            this.GameSettings_Tab_Graphics.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameSettings_TabCtrl
+            // 
+            this.GameSettings_TabCtrl.Controls.Add(this.GameSettings_Tab_Graphics);
+            this.GameSettings_TabCtrl.Controls.Add(this.GameSettings_Tab_Sound);
+            this.GameSettings_TabCtrl.Location = new System.Drawing.Point(12, 12);
+            this.GameSettings_TabCtrl.Name = "GameSettings_TabCtrl";
+            this.GameSettings_TabCtrl.SelectedIndex = 0;
+            this.GameSettings_TabCtrl.Size = new System.Drawing.Size(776, 376);
+            this.GameSettings_TabCtrl.TabIndex = 0;
+            // 
+            // GameSettings_Tab_Graphics
+            // 
+            this.GameSettings_Tab_Graphics.BackColor = System.Drawing.Color.LightGray;
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox7);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox6);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox5);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.trackBar4);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label8);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox4);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox3);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox2);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.checkBox1);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.trackBar3);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label7);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.trackBar2);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label6);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.trackBar1);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.comboBox4);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label5);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label4);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label3);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.comboBox3);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.comboBox2);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label2);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.label1);
+            this.GameSettings_Tab_Graphics.Controls.Add(this.comboBox1);
+            this.GameSettings_Tab_Graphics.Location = new System.Drawing.Point(4, 22);
+            this.GameSettings_Tab_Graphics.Name = "GameSettings_Tab_Graphics";
+            this.GameSettings_Tab_Graphics.Padding = new System.Windows.Forms.Padding(3);
+            this.GameSettings_Tab_Graphics.Size = new System.Drawing.Size(768, 350);
+            this.GameSettings_Tab_Graphics.TabIndex = 0;
+            this.GameSettings_Tab_Graphics.Text = "Graphics";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(286, 115);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(283, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Shadow Detail";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(286, 67);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(283, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Vehicle Detail";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(286, 19);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(131, 59);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(82, 21);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Colors";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Draw Distance";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Renderer";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(9, 99);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(204, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(9, 59);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(82, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aspect Ratio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Renderer";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Direct 3D 7",
+            "OpenGL",
+            "Software",
+            "Glide"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // GameSettings_Tab_Sound
+            // 
+            this.GameSettings_Tab_Sound.BackColor = System.Drawing.Color.LightGray;
+            this.GameSettings_Tab_Sound.Location = new System.Drawing.Point(4, 22);
+            this.GameSettings_Tab_Sound.Name = "GameSettings_Tab_Sound";
+            this.GameSettings_Tab_Sound.Padding = new System.Windows.Forms.Padding(3);
+            this.GameSettings_Tab_Sound.Size = new System.Drawing.Size(768, 350);
+            this.GameSettings_Tab_Sound.TabIndex = 1;
+            this.GameSettings_Tab_Sound.Text = "Sound";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(689, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Revert";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 143);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Mipmap";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(9, 166);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(99, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Bilinear Filtering";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(9, 221);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(75, 17);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "Lens Flare";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(9, 244);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(87, 17);
+            this.checkBox4.TabIndex = 16;
+            this.checkBox4.Text = "Water Wake";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(287, 160);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(284, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Texture Resolution";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(9, 267);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(76, 17);
+            this.checkBox5.TabIndex = 19;
+            this.checkBox5.Text = "Headlights";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(9, 188);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(99, 17);
+            this.checkBox6.TabIndex = 20;
+            this.checkBox6.Text = "Bilinear Filtering";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(9, 290);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(79, 17);
+            this.checkBox7.TabIndex = 21;
+            this.checkBox7.Text = "Reflections";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // GameSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GameSettings_TabCtrl);
+            this.Name = "GameSettings";
+            this.Text = "Game Settings";
+            this.GameSettings_TabCtrl.ResumeLayout(false);
+            this.GameSettings_Tab_Graphics.ResumeLayout(false);
+            this.GameSettings_Tab_Graphics.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl GameSettings_TabCtrl;
+        private System.Windows.Forms.TabPage GameSettings_Tab_Graphics;
+        private System.Windows.Forms.TabPage GameSettings_Tab_Sound;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ 4x4EvoLauncher/4x4Evo_Launcher/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _4x4Evo_Launcher
+{
+    public partial class GameSettings : Form
+    {
+        public GameSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
